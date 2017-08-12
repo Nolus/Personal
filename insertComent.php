b90e31846efe269875059ed3680f499d649b9b39
Nolus <israelnolust@hotmail.com>
2017-08-12 12:36:54 -0400

    correcion formulario
    php no tomaba valores del formulario debido a que no estaba entre las etiquetas de div, ahora php coje los valores pero es necesario utilizar jquery para refirescar la pagina automaticamente sin estar en la pagina php

--- insertComent.php
+++ insertComent.php
@@ -4,35 +4,39 @@
 		$password = "";
 		$dbname = "sonidodepipocas";
 
-		$nombre = trim(filter_input(INPUT_POST, "nombre"));
-		$email = trim(filter_input(INPUT_POST, "email"));
-		$sugerencia = trim(filter_input(INPUT_POST, "sugerencia"));
+		error_reporting(E_ALL);
+		ini_set('error_reporting', E_ALL);
 
-		verificarEmail($username,$password,$nombre,$email,$sugerencia);
+		if (isset($_POST["submit"])) {
 
-		function verificarEmail ($username, $password, $nombre, $email, $sugerencia){
-		    //if (filter_var($email, FILTER_VALIDATE_EMAIL)){
-		        //echo "Gracias por escribir, que tengas un excelente resto de jornada";
-				try {
-				    $con = new PDO("mysql:host=localhost;dbname=sonidodepipocas", $username, $password);
-				    // set the PDO error mode to exception
-				    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				    $query = $con->prepare("INSERT INTO sugerencias_enlaces (nombre, email, sugerencia) VALUES (:nombre, :email, :sugerencia");
-				    $query->bindParam(":nombre",$nombre);
-				    $query->bindParam(":email",$email);
-				    $query->bindParam(":sugerencia",$sugerencia);
-				    $query->execute();
-				    //header("refresh:4; index.html");
-				    echo "Gracias por escribir, que tengas un excelente resto de jornada";
-				    }catch(PDOException $e){
-				    	echo $con . "<br>" . $e->getMessage();
-				    }
-					$con = null;
-		    //}else{
-		    //    echo $email. " no es un correo valido, intenta otra vez\n";
-		   //     header("refresh:4; contacto.html");
-		   // }
-		}
+
+			$nombre = trim(filter_input(INPUT_POST, "nombre"));
+			$email = trim(filter_input(INPUT_POST, "email"));
+			$sugerencia = trim(filter_input(INPUT_POST, "sugerencia"));
+			
+
+			echo "nombre:".$nombre.", email:".$email.", sugerencia:".$sugerencia;
+
+					try {
+					    $con = new PDO("mysql:host=localhost;dbname=sonidodepipocas", $username, $password);
+					    // set the PDO error mode to exception
+					    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					    $query = $con->prepare("INSERT INTO sugerencias_enlaces (nombre, email, sugerencia) VALUES (:nombre, :email, :sugerencia)");
+					    $query->bindParam(":nombre",$nombre);
+					    $query->bindParam(":email",$email);
+					    $query->bindParam(":sugerencia",$sugerencia);
+					    $query->execute();
+					    //header("refresh:4; index.html");
+					    echo "Gracias por escribir, que tengas un excelente resto de jornada";
+					    }catch(PDOException $e){
+					    	echo $con . "<br>" . $e->getMessage();
+					    }
+						$con = null;
+			
+				
+			} else {
+				echo "error";
+			}
 
 ?>
 
